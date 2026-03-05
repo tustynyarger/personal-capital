93332441e6d1d200f3b68d5ca164abf8f6b483d2
Tustyn Yarger <tustynyarger@Tustyns-MacBook-Air-4.local>
2026-03-05 12:11:54 -0500

    upgrade next to fix security issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & {
+  eslint: {
+    ignoreDuringBuilds: boolean;
+  };
+} = {
   eslint: {
     ignoreDuringBuilds: true,
   },
